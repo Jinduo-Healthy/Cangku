aabdc208a606ae6afd71a55c7f7b9f244112408a
ZJQ <zjq@zjqdemacbook-pro.local>
2018-10-15 09:53:51 +0800

    Initial Commit

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by ZJQ on 2018/10/15.
+//  Copyright © 2018年 WXM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestGit
+//
+//  Created by ZJQ on 2018/10/15.
+//  Copyright © 2018年 WXM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
